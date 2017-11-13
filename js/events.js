a35898c2c8a407c3a56acbe89d4c3f2c9bb69fca
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-13 15:48:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -2,29 +2,29 @@
 function getIt() {
   $('p').on('click',function(){
     alert('Hey!')
-  })
-};
+  });
+}
 
 function frameIt() {
   $('img').on('load', function () {
     $('img').addClass('tasty');
-  })
-};
+  });
+}
 
 function pressIt() {
   $('#typing').on('keydown',function (key) {
     if(key.which === 71){
       alert('Yo');
     }
-  })
-};
+  });
+}
 
 function submitIt() {
   $('form').on('submit',function () {
     alert("Your form is going to be submitted now.");
     return;
-  })
-};
+  });
+}
 
 $(document).ready(function(){
 // call functions here
